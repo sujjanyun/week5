81831fb6fe9399d8e114effe6252f682edc863ba
Susan Yun <susanhkyun@gmail.com>
2021-01-12 13:56:17 -0600

    before modal

--- chuck_2021/js/get.js
+++ chuck_2021/js/get.js
@@ -1,17 +1,15 @@
 'use strict';
 
 function get(url) {
-    
+    return fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            return data;
+        })
+        .catch(function (error) {
+            console.error('ERROR:', error);
+            return error;
+        });
 }
-fetch('https://api.chucknorris.io/jokes/random?category=dev')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        updateBody(data.value);
-        return data;
-    })
-    .catch(function (error) {
-        console.error("ERROR:", error);
-        return error;
-    });
